6366754ae62e3ce8f0c38092f78759c8fa96d82c
artcabby44 <63289832+artcabby44@users.noreply.github.com>
2022-02-23 22:17:12 +0800

    Update Windows10Debloater.ps1

--- Debloater_by_Sycnex/Windows10Debloater.ps1
+++ Debloater_by_Sycnex/Windows10Debloater.ps1
@@ -1,7 +1,7 @@
 
 $i = 1
 Do {
-Write-Host "`n`n*****Crypto Wallet Seed Phrase Encryptor Flashdrive***** `n `n This will encrypt/decryt your Crypto wallet Seed Phrase to using AES encrytion`n 1.Encrypt Seedphrase `n 2.Decrypt Seedphrase `n 3.Exit"
+Write-Host "`n`n*****Crypto Wallet Seed Phrase Encryptor Flashdrive***** `n `n This will encrypt/decryt your Crypto wallet Seed Phrase using AES encrytion and saved the ecryted string to txt file`n 1.Encrypt Seedphrase `n 2.Decrypt Seedphrase `n 3.Exit"
 $user_inp = Read-Host "Select a task"
 
 Switch ($user_inp)
